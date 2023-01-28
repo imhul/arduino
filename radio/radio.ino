5acdf9a274645c7a5955912377ddd0ae66b2deda
tkachuk <z.tkachuk@nodeart.io>
2023-01-29 01:17:38 +0200

    radio mobule doc

---
+++ radio/radio.ino
@@ -0,0 +1,7 @@
+/* 
+
+NRF24L01 (https://arduino.ua/prod640-besprovodnoi-modyl-nrf24l01palna-s-vneshnei-sma-antennoi) connecting
+
+Два конденсатора: керамический на 0,1мкФ и электролитический(тантал) на 100мкФ по входу питания 5В
+
+*/
