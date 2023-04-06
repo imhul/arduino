888efeab6c9e48951466aafb831044b70c91e77a
tkachuk <z.tkachuk@nodeart.io>
2023-04-07 01:54:25 +0300

    add dc motors driver

--- RX/RX.ino
+++ RX/RX.ino
@@ -2,11 +2,24 @@
 #include "nRF24L01.h"
 #include "RF24.h"
 #include "Servo.h"
+#include "GyverMotor2.h"  // https://github.com/GyverLibs/GyverMotor/blob/main/examples/GyverMotor2/demo/demo.ino
+#include "TimeOut.h"
 
+// timer
+#define SECOND 1000UL
+#define MINUTE (60 * SECOND)
+TimeOut timeout;
+
+// DC motors
+GMotor2<DRIVER2WIRE> motor(6, 5);
+int speed = 0;
+
+// radio
 const uint64_t pipe = 0xF0F1F2F3F4LL;  // transmission identifier
 RF24 radio(7, 8);                      // For MEGA2560 radio(9,53);
 byte recieved_data[7];
 
+// servo
 Servo tail;
 Servo leftWingServo;
 Servo rightWingServo;
@@ -19,8 +32,9 @@ int leftWingAngle = 0;
 int rightWingAngle = 0;
 int tailAngle = 0;
 
-int motors = 9;  // D9
-int speed = 0;
+// state
+bool printed = false;
+bool noSignal = false;
 
 void setup() {
   Serial.begin(9600);
@@ -52,30 +66,33 @@ void setup() {
   delay(1000);
 
   // set motors
-  pinMode(motors, OUTPUT);
-  // analogWrite(motors, speed);
-  delay(1000);
+  motor.setMinDuty(70);
   Serial.println("Motors init!");
   Serial.println("-----------------------");
-  delay(1000);
+  delay(2000);
 }
 
 void printResponse(byte data[7]) {
-  Serial.print("button left: ");
-  Serial.println(recieved_data[0]);
-  Serial.print("button right: ");
-  Serial.println(recieved_data[1]);
-  Serial.print("potent (motor speed): ");
-  Serial.println(recieved_data[2]);
-  Serial.print("X left: ");
-  Serial.println(recieved_data[3]);
-  Serial.print("Y left: ");
-  Serial.println(recieved_data[4]);
-  Serial.print("X right: ");
-  Serial.println(recieved_data[5]);
-  Serial.print("Y right: ");
-  Serial.println(recieved_data[6]);
-  Serial.println("-----------------------");
+  if (!printed) {
+    printed = true;
+    Serial.print("button left: ");
+    Serial.println(recieved_data[0]);
+    Serial.print("button right: ");
+    Serial.println(recieved_data[1]);
+    Serial.print("potent (motor speed): ");
+    Serial.println(recieved_data[2]);
+    Serial.print("X left: ");
+    Serial.println(recieved_data[3]);
+    Serial.print("Y left: ");
+    Serial.println(recieved_data[4]);
+    Serial.print("X right: ");
+    Serial.println(recieved_data[5]);
+    Serial.print("Y right: ");
+    Serial.println(recieved_data[6]);
+    Serial.println("-----------------------");
+    delay(1000);
+    printed = false;
+  }
 }
 
 void moveTail(int value) {
@@ -113,7 +130,7 @@ void moveTail(int value) {
 
 void moveLeftWing(int value) {
   leftWingServoState = value;
-  
+
   if (leftWingServoState > 45) {
     // up
     for (leftWingAngle; leftWingAngle < leftWingServoState; leftWingAngle++) {
@@ -173,28 +190,46 @@ void moveRightWing(int value) {
   }
 }
 
+void rotateMotors(int value) {
+  speed = value;
+  motor.setSpeed(speed);
+}
+
+void returnAction() {
+  noSignal = false;
+  Serial.println("Return action!");
+  delay(2000);
+}
+
+void noSignalActon() {
+  if (noSignal) return;
+  noSignal = true;
+  Serial.println("No signal!");
+  // TODO: timer
+  timeout.timeOut(2000, returnAction);
+}
+
 void loop() {
   if (radio.available()) {
     radio.read(&recieved_data, sizeof(recieved_data));
+    // timer
+    timeout.cancel();
+
+    if (speed != recieved_data[2]) rotateMotors(recieved_data[2]);
+
+    // TODO: adjustment is required: if (tailState != recieved_data[4] && tailState + 5 < recieved_data[4] && tailState - 5 > recieved_data[4]) {
+    if (tailState != recieved_data[4]) moveTail(recieved_data[4]);
+
+    // TODO: adjustment is required: if (rightWingServoState != recieved_data[5] && rightWingServoState + 5 < recieved_data[5] && rightWingServoState - 5 > recieved_data[5]) {
+    if (rightWingServoState != recieved_data[5]) moveRightWing(recieved_data[5]);
+
+    // TODO: adjustment is required: if (leftWingServoState != recieved_data[3] && leftWingServoState + 5 < recieved_data[3] && leftWingServoState - 5 > recieved_data[3]) {
+    if (leftWingServoState != recieved_data[3]) moveLeftWing(recieved_data[3]);
+
     printResponse(recieved_data);
 
-    speed = recieved_data[2];
-
-    if (tailState != recieved_data[4]) {
-      moveTail(recieved_data[4]);
-    }
-
-    if (rightWingServoState != recieved_data[5]) {
-      moveRightWing(recieved_data[5]);
-    }
-
-    if (leftWingServoState != recieved_data[3]) {
-      moveLeftWing(recieved_data[3]);
-    }
-
   } else {
-    Serial.println("No signal!");
+    noSignalActon();
+    timeout.handler();
   }
-
-  analogWrite(motors, speed);
 }
